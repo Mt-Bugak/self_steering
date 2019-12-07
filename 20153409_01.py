6987508d18ed13fe869c3ba5f0654b7da1b1bd39
Mt-Bugak <48546277+Mt-Bugak@users.noreply.github.com>
2019-12-07 23:12:50 +0900

    Update 20153409_01.py

--- 20153409_01.py
+++ 20153409_01.py
@@ -1,7 +1,7 @@
 from tensorflow import keras
 import h5py
+import os, cv2
 import argparse
-import os
 
 parser = argparse.ArgumentParser(description='python 20153409_3.py --input_dir ./input_data_folder --output_dir ./output_data_folder')
 parser.add_argument('--input_dir', type=str, help='put an input directory')
@@ -12,6 +12,7 @@ input_list = os.listdir(args.input_dir)
 
 model = keras.models.load_model('20153409_01.h5')
 for i in input_list:
-  img = args.input_dir+i
+  if(i[0]=='.'): continue
+  img = cv2.imread(args.input_dir+'/'+i)
   predicted_steers = model.predict(img[None, :, :, :].transpose(0, 3, 1, 2))[0][0]
   print(round(predicted_steers,5))
